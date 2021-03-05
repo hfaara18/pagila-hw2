65d2ea4c9bd5e2efb47b24d1657babadeecf8725
Hussein Faara <faarahussein@gmail.com>
2021-03-04 22:44:06 -0800

    passed 07.sql

--- sql/07.sql
+++ sql/07.sql
@@ -10,3 +10,17 @@
  * This can be solved either with a LEFT JOIN or with the NOT IN operator.
  * You may choose whichever solution makes the most sense to you.
  */
+select distinct title
+from film
+join inventory using (film_id)
+where inventory_id is not null and film_id not in (
+    select film_id
+    from inventory
+    join rental using (inventory_id)
+    join customer using (customer_id)
+    join address using (address_id)
+    join city using (city_id)
+    join country using (country_id)
+    where country='United States'
+)
+order by title;
